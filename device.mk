3bfe512188e63b43828eb6e203723bed53b78f97
ZIDAN44 <zidan.roking@gmail.com>
2022-10-06 19:39:40 +0530

    RMX1851: Guard qti thermal 2.0 service hals * Its crashes on liber but fine on other devices
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    Change-Id: Idabe84f3f116fba27e4e9c6e5a9f6e840db20041

--- device.mk
+++ device.mk
@@ -433,8 +433,10 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
    android.hardware.thermal@2.0-service.qti
+endif
 
 # Touch
 PRODUCT_PACKAGES += \
