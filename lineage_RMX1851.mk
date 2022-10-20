0cd2a386c6e444e29e877881ddcde260a84f6db4
Cykeek <Cykeek@proton.me>
2022-10-20 22:27:44 +0530

    RMX1851: Add Pixel7Pro CTS Fingerprint
     * Improve properties code style
    
     * Resolves Android P-DP1 "Android system" warning:
        "There's an internal problem with your device.
         Contact your manufacturer for details."
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- lineage_RMX1851.mk
+++ lineage_RMX1851.mk
@@ -25,10 +25,10 @@ TARGET_SUPPORTS_QUICK_TAP := true
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220624.021/8877034:user/release-keys"
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TD1A.220804.031/9071314:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220624.021 8877034 release-keys" \
+    PRIVATE_BUILD_DESC="cheetah-user 13 TD1A.220804.031 9071314 release-keys" \
     PRODUCT_NAME="RMX1851"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
