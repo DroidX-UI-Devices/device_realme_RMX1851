91e0fed16d3561c2b35a2a6021166461f83af788
cykeek <cykeek@proton.me>
2023-02-03 17:35:48 +0000

    RMX1851: Build HIDL vendor variants Fixes stuff like: CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.oppo.hardware.ktv@1.0-service": library "android.hidl.base@1.0.so" not found: needed by main executable
    Signed-off-by: cykeek <cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -258,9 +258,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system \
+    android.hidl.manager@1.0_vendor \
     libhidltransport \
     libhwbinder \
     libhidltransport.vendor \
