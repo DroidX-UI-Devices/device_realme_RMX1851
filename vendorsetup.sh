55315c12dd6f65a73aa032139ffee22e4560944d
cykeek <cykeek@proton.me>
2023-02-16 08:40:53 +0000

    RMx1851: Stop tracking hals *every source fuckedup on their own
    Signed-off-by: cykeek <cykeek@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,15 +24,3 @@ git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 kernel/realme/sdm7
 banner 2>1
 echo "Cloning RealmeParts..."
 git clone https://github.com/Cykeek-Labs/packages_apps_realmeparts packages/apps/RealmeParts
-
-# Clone Hals
-banner 2>1
-echo "Cleaning prebuilt Hals...."
-rm -rf hardware/qcom-caf/sdm845/audio
-rm -rf hardware/qcom-caf/sdm845/media
-rm -rf hardware/qcom-caf/sdm845/display
-sleep 1s
-echo "Cloning hals from Lineage..."
-git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom-caf/sdm845/audio --depth=1
-git clone https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom-caf/sdm845/media --depth=1
-git clone https://github.com/VoltageOS/hardware_qcom_display hardware/qcom-caf/sdm845/display --depth=1
